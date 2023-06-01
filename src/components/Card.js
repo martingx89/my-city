f1c9261517d6f1ad33e4c3b35dc064a3684356bb
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-06-01 08:40:24 +0000

    Prepare headers

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,13 @@
+// import styles from './Card.module.scss';
+
 const Card = () => {
-  return <h1>Track</h1>;
+  return (
+    <div>
+      <img src='' alt='map image track' />
+      <h1>Track</h1>
+      <h2>No 1</h2>
+    </div>
+  );
 };
 
 export default Card;
